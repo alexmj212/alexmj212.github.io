5f992c0ee70b79ded4654036b183d4b5fedfb25a
Alex Johnson <alexmj212@gmail.com>
2021-12-05 20:38:46 -0500

    improve tailwind styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,59 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: "media", // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: {
+        DEFAULT: colors.trueGray[700],
+        dark: colors.trueGray[900],
+      },
+      white: {
+        DEFAULT: colors.white,
+        dark: colors.trueGray[200],
+      },
+      blue: colors.blue,
+      gray: colors.trueGray,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber,
+      accent1: {
+        DEFAULT: "var(--carolina-blue)",
+        dark: "var(--carolina-blue-dark)",
+      },
+      accent2: {
+        DEFAULT: "var(--russian-green)",
+        dark: "var(--russian-green-dark)",
+      },
+      accent3: {
+        DEFAULT: "var(--ruby-red)",
+        dark: "var(--ruby-red-dark)",
+      },
+      panel: {
+        DEFAULT: colors.white,
+        dark: colors.trueGray[800],
+      }
+    },
+    fontFamily: {
+      serif: ["Georgia", "serif"],
+      sans: ["Work Sans", "sans-serif"],
+      sans2: ["Open Sans", "sans-serif"],
+      condensed: ["Work Sans", "sans-serif"],
+      header: ["Work Sans", "sans-serif"],
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
