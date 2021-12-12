5e30c438903a11e3a96d9e5a7f451e4e10baebab
Alex Johnson <alexmj212@gmail.com>
2021-12-12 01:06:02 -0500

    gatsby init

---
+++ blog/gatsby-config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "alexmj212.dev blog",
+  },
+  plugins: [
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-plugin-mdx",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
