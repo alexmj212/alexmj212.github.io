87fd35738f6fa885c0486704fa1880f61912545c
Alex Johnson <alexmj212@gmail.com>
2022-01-16 16:43:56 -0500

    improve base styles

--- src/components/experience/Experience.tsx
+++ src/components/experience/Experience.tsx
@@ -19,13 +19,12 @@ const Experience = () => {
               <p className={`text-accent2 mb-0 font-semibold`}>{experienceItem.date}</p>
             </div>
             <div className={``}>
-              <h2 className={`mb-0`}>{experienceItem.title}</h2>
-              <h3>{experienceItem.company}</h3>
+              <h2 className={`my-2`}>{experienceItem.title}</h2>
+              <h3 className={`my-2`}>{experienceItem.company}</h3>
               <List>{experienceItem.summary && experienceItem.summary.map((bullet) => <ListItem key={bullet}>{bullet}</ListItem>)}</List>
               {experienceItem.projects &&
                 experienceItem.projects.map((project) => (
                   <div key={project.name} className={`mb-4`}>
-                    <h4>{project.name}</h4>
                     <List horizontal className={`no-style`}>
                       {project.skills.map((skill) => (
                         <ListItem key={skill}>
